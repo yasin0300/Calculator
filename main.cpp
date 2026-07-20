fc08eccc1c89538ca25421a4de498fdd5a0626a7
Yasin Arpaci <132942379+yasin0300@users.noreply.github.com>
2026-07-21 00:36:00 +0200

    Improve runtime and fixed bugs

--- main.cpp
+++ main.cpp
@@ -27,65 +27,72 @@ int main()
     while (w == 1)
     {
         std::cout << "Welche Art von Rechnung moechten sie durchfuehren?" << std::endl;
-        std::string operatoren[] = {"1(+)  ", "2(-)  ", "3(*)  ", "4(/)"};
-        for (int i = 0; i < 4; i++)
-        {
-            std::cout << operatoren[i];
-        }
-        std::cout << "" << std::endl;
+        std::cout << "1(+)  ";
+        std::cout << "2(-)  ";
+        std::cout << "3(*)  ";
+        std::cout << "4(/)  " << std::endl;
+
         int x;
         std::cin >> x;
-        if (x == 1)
+        if (std::cin.fail())
         {
-            std::cout << "Additon" << std::endl;
-            std::cout << "Geben sie 2 Zahlen ein\n";
-            int num1, num2;
-            std::cin >> num1 >> num2;
-            int Summe = addi(num1, num2);
-            std::cout << Summe << std::endl;
-        }
-        else if (x == 2)
-        {
-            std::cout << "Subtraktion" << std::endl;
-            std::cout << "Geben sie 2 Zahlen ein\n";
-            int num1, num2;
-            std::cin >> num1 >> num2;
-            int Differenz = subt(num1, num2);
-            std::cout << Differenz << std::endl;
-        }
-        else if (x == 3)
-        {
-            std::cout << "Multiplikation" << std::endl;
-            std::cout << "Geben sie 2 Zahlen ein\n";
-            int num1, num2;
-            std::cin >> num1 >> num2;
-            int Produkt = mult(num1, num2);
-            std::cout << Produkt << std::endl;
-        }
-        else if (x == 4)
-        {
-            std::cout << "Division" << std::endl;
-            std::cout << "Geben sie 2 Zahlen ein\n";
-            int num1, num2;
-            std::cin >> num1 >> num2;
-
-            if (num2 == 0)
-            {
-                std::cout << "Sie koennen nicht durch 0 teilen" << std::endl;
-            }
-            else
-            {
-                int quotient = divi(num1, num2);
-                std::cout << quotient << std::endl;
-            }
+            std::cout << "Ungueltige Eingabe";
         }
         else
         {
-            std::cout << "Ungueltige Eingabe" << std::endl;
-        }
 
-        std::cout << "Weiter(1) beenden(0)" << std::endl;
-        std::cin >> w;
+            if (x == 1)
+            {
+                std::cout << "Additon" << std::endl;
+                std::cout << "Geben sie 2 Zahlen ein\n";
+                int num1, num2;
+                std::cin >> num1 >> num2;
+                int Summe = addi(num1, num2);
+                std::cout << Summe << std::endl;
+            }
+            else if (x == 2)
+            {
+                std::cout << "Subtraktion" << std::endl;
+                std::cout << "Geben sie 2 Zahlen ein\n";
+                int num1, num2;
+                std::cin >> num1 >> num2;
+                int Differenz = subt(num1, num2);
+                std::cout << Differenz << std::endl;
+            }
+            else if (x == 3)
+            {
+                std::cout << "Multiplikation" << std::endl;
+                std::cout << "Geben sie 2 Zahlen ein\n";
+                int num1, num2;
+                std::cin >> num1 >> num2;
+                int Produkt = mult(num1, num2);
+                std::cout << Produkt << std::endl;
+            }
+            else if (x == 4)
+            {
+                std::cout << "Division" << std::endl;
+                std::cout << "Geben sie 2 Zahlen ein\n";
+                int num1, num2;
+                std::cin >> num1 >> num2;
+
+                if (num2 == 0)
+                {
+                    std::cout << "Sie koennen nicht durch 0 teilen" << std::endl;
+                }
+                else
+                {
+                    int quotient = divi(num1, num2);
+                    std::cout << quotient << std::endl;
+                }
+            }
+            else
+            {
+                std::cout << "Ungueltige Eingabe" << std::endl;
+            }
+
+            std::cout << "Weiter(1) beenden(0)" << std::endl;
+            std::cin >> w;
+        }
+        return 0;
     }
-    return 0;
 }
