8eaba635788f83f442e6b0d4d31ac16943809465
Yasin <yasin.arpaci09@gmail.com>
2026-07-24 23:54:01 +0200

    fixed stuff

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <string>
 
-double addi(double summant1, double summant2)
+double validateInput()
 {
-    return summant1 + summant2;
+    double input;
+    while (true)
+    {
+        std::cin >> input;
+        if (std::cin.fail())
+        {
+            std::cout << "Invalid input! Please enter a valid number: " << std::endl;
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+        }
+        else
+        {
+            return input;
+        }
+    }
+}
+
+double addi(double summand1, double summand2)
+{
+    return summand1 + summand2;
 }
 
 double subt(double minuend, double subtrahend)
@@ -11,14 +30,14 @@ double subt(double minuend, double subtrahend)
     return minuend - subtrahend;
 }
 
-double mult(double faktor1, double faktor2)
+double mult(double factor1, double factor2)
 {
-    return faktor1 * faktor2;
+    return factor1 * factor2;
 }
 
-double divi(double divident, double divisor)
+double divi(double dividend, double divisor)
 {
-    return divident / divisor;
+    return dividend / divisor;
 }
 
 int main()
@@ -26,85 +45,79 @@ int main()
     int w = 1;
     while (w == 1)
     {
-        std::cout << "Welche Art von Rechnung moechten sie durchfuehren?" << std::endl;
+        std::cout << "What kind of calculation would you like to perform??" << std::endl;
         std::cout << "1(+)  ";
         std::cout << "2(-)  ";
         std::cout << "3(*)  ";
         std::cout << "4(/)" << std::endl;
 
-        bool gultigeeingabe = false;
+        bool validInput = false;
         int x;
-        while (!gultigeeingabe)
+        while (!validInput)
         {
-            std::cin >> x;
-            if (std::cin.fail())
+            x = validateInput();
+            validInput = true;
+            switch (x)
             {
-                std::cout << "Ungueltige Eingabe!" << std::endl;
-                std::cin.clear();
-                std::cin.ignore(1000, '\n');
-                std::cout << "Bitte geben sie eine gueltige Zahl ein" << std::endl;
+            case 1:
+            {
+                std::cout << "Addition" << std::endl;
+                std::cout << "Enter 2 numbers\n";
+                double num1, num2;
+                num1 = validateInput();
+                num2 = validateInput();
+                double Summe = addi(num1, num2);
+                std::cout << Summe << std::endl;
+                break;
             }
-            else
+            case 2:
             {
-                gultigeeingabe = true;
-                switch (x)
-                {
-                case 1:
-                {
-                    std::cout << "Additon" << std::endl;
-                    std::cout << "Geben sie 2 Zahlen ein\n";
-                    double num1, num2;
-                    std::cin >> num1 >> num2;
-                    double Summe = addi(num1, num2);
-                    std::cout << Summe << std::endl;
-                    break;
-                }
-                case 2:
-                {
-                    std::cout << "Subtraktion" << std::endl;
-                    std::cout << "Geben sie 2 Zahlen ein\n";
-                    double num1, num2;
-                    std::cin >> num1 >> num2;
-                    double Differenz = subt(num1, num2);
-                    std::cout << Differenz << std::endl;
-                    break;
-                }
-                case 3:
-                {
-                    std::cout << "Multiplikation" << std::endl;
-                    std::cout << "Geben sie 2 Zahlen ein\n";
-                    double num1, num2;
-                    std::cin >> num1 >> num2;
-                    double Produkt = mult(num1, num2);
-                    std::cout << Produkt << std::endl;
-                    break;
-                }
-                case 4:
-                {
-                    std::cout << "Division" << std::endl;
-                    std::cout << "Geben sie 2 Zahlen ein\n";
-                    double num1, num2;
-                    std::cin >> num1 >> num2;
+                std::cout << "Subtraction" << std::endl;
+                std::cout << "Enter 2 numbers\n";
+                double num1, num2;
+                num1 = validateInput();
+                num2 = validateInput();
+                double Differenz = subt(num1, num2);
+                std::cout << Differenz << std::endl;
+                break;
+            }
+            case 3:
+            {
+                std::cout << "Multiplication" << std::endl;
+                std::cout << "Enter 2 numbers\n";
+                double num1, num2;
+                num1 = validateInput();
+                num2 = validateInput();
+                double Produkt = mult(num1, num2);
+                std::cout << Produkt << std::endl;
+                break;
+            }
+            case 4:
+            {
+                std::cout << "Division" << std::endl;
+                std::cout << "Enter 2 numbers\n";
+                double num1, num2;
+                num1 = validateInput();
+                num2 = validateInput();
 
-                    if (num2 == 0)
-                    {
-                        std::cout << "Sie koennen nicht durch 0 teilen" << std::endl;
-                    }
-                    else
-                    {
-                        double quotient = divi(num1, num2);
-                        std::cout << quotient << std::endl;
-                    }
-                    break;
+                if (num2 == 0)
+                {
+                    std::cout << "You cannot divide by 0" << std::endl;
                 }
-                default:
-                    std::cout << "Ungueltige Eingabe" << std::endl;
-                    break;
+                else
+                {
+                    double quotient = divi(num1, num2);
+                    std::cout << quotient << std::endl;
                 }
-                std::cout << "Weiter(1) beenden(0)" << std::endl;
-                std::cin >> w;
+                break;
             }
+            default:
+                std::cout << "Invalid input" << std::endl;
+                break;
+            }
+            std::cout << "Continue(1) exit(0)" << std::endl;
+            w = validateInput();
         }
     }
     return 0;
-} 
+}
